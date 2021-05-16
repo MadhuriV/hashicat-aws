bdd47de8c43673fdd061fc9753e4a1e1e460cb70
Madhuri Velaga <madhuri.velaga@santander.co.uk>
2021-05-16 20:15:59 +0000

    resolve conflict

--- remote_backend.tf
+++ remote_backend.tf
@@ -5,11 +5,7 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-<<<<<<< HEAD
     organization = "Madhuri-Terra-Training"
-=======
-    organization = "Madhuri-Training"
->>>>>>> 6a409c4a4f50e690712c0e8a82603e7b160b6694
     workspaces {
       name = "hashicat-aws"
     }
